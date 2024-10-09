e9eeedebfee77d27eeabedf08a07f6ac4b5bd108
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-10-09 08:42:46 -0300

    Implement the payment micro service

---
+++ services/payment/src/main/java/com/devsouzx/payment/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.devsouzx.payment.notification;
+
+import com.devsouzx.payment.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
